23a19de91ab8e3f73782fb020ba57d92037a3898
Thomas Kambadzi <tkambadzi3@gmail.com>
2023-05-02 22:34:08 +0000

    Adding my exercises files for the Variables chapter

---
+++ variables/name.rb
@@ -0,0 +1,21 @@
+# Solution: problem 1 
+
+puts "What is your name?"
+name = gets.chomp
+puts "Hi #{name}, how has been your day?"
+
+# Solution: problem 3
+puts "What is your name?"
+name = gets.chomp
+10.times {puts name}
+
+# Solution: problem 4
+
+puts "What is your name?"
+name = gets.chomp
+puts "What's your last name?"
+last_name = gets.chomp
+puts "Your full name is" + " " + name + " " + last_name
+
+# Solution: problem 5
+
